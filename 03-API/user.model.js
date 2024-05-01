371ab79ed12aea9da65512d7775cd2d7278d992d
ghanigh <rhani.94@proton.me>
2024-05-01 13:11:05 +0200

    test

--- 03-API/user.model.js
+++ 03-API/user.model.js
@@ -3,7 +3,8 @@ import uniqueValidator from "mongoose-unique-validator";
 
 const userSchema = mongoose.Schema(
     {
-        firstname: { type: String, required: true },
+       /*  firstname: { type: String, required: true }, */
+       
         picture: { type: String, required: true },
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true }
